dfcf175569bc2540043f983a81b1449f0c921419
Prateek Narayanan <prnaraya@ucsc.edu>
2024-02-19 11:56:27 -0800

    Added /api to http-common header

--- react/src/http-common.js
+++ react/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://20.66.98.62:8000/",
+  baseURL: "http://20.66.98.62:8000/api",
   headers: {
     "Content-type": "application/json"
   }
